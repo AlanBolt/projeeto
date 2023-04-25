547e32544c9350fa0f6caabb4aa554d4e0609520
Alan.Barros <126121063+AlanBolt@users.noreply.github.com>
2023-04-25 20:46:16 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+
+const bota = document.querySelector("#btn1");
+function hello(){
+  console.log("oi");
+}
+function word(){
+  console.log("gente");
+}
+//adcionar mecanica
+bota.addEventListener("click", hello);
+bota.addEventListener("click", word);
+
+
+
+const bot = document.querySelector("#btn2");
+bot.addEventListener("click", ()=>{
+  hello();
+
+  word();
+}); 
